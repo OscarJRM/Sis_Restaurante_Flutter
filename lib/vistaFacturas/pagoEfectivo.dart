382b11f21240afd11cf4a9aa429fa76519daab61
David Lopez <dl735894@gmail.com>
2024-01-15 11:04:11 -0500

    se actualizo la insercion en la tabla facturas

--- lib/vistaFacturas/pagoEfectivo.dart
+++ lib/vistaFacturas/pagoEfectivo.dart
@@ -272,7 +272,7 @@ class _PagoEfectivoViewState extends State<PagoEfectivoView> {
           // Agrega otras columnas según sea necesario
         ];
       }).toList();
-      
+
       // Crear el PDF
       final pdf = pw.Document();
 
@@ -323,8 +323,8 @@ class _PagoEfectivoViewState extends State<PagoEfectivoView> {
       // Actualizar la base de datos con la ruta del PDF
       final pdfPath = pdfFile.path;
       await connection.execute(
-        "INSERT INTO facturas (ced_cli, id_pag, monto_total, fecha_emision, pdf_path) VALUES "
-        "('${cedulaController.text}', 1, ${globalState.Total}, '${DateTime.now().toLocal()}', '$pdfPath')",
+        "INSERT INTO facturas (ced_cli, id_pag, monto_total, fecha_emision, pdf_path, id_ped_per) VALUES "
+        "('${cedulaController.text}', 1, ${globalState.Total}, '${DateTime.now().toLocal()}', '$pdfPath', ${globalState.idPed})",
       );
 
       await connection.close();
